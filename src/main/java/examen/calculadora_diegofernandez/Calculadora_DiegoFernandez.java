e6139869144a00c1f48cb1416a2560d388b97869
GogetaSS4 <diego.fernandezfernandez@iesmiguelherrero.com>
2023-01-13 17:34:31 +0100

    repositorio

--- src/main/java/examen/calculadora_diegofernandez/Calculadora_DiegoFernandez.java
+++ src/main/java/examen/calculadora_diegofernandez/Calculadora_DiegoFernandez.java
@@ -12,6 +12,6 @@ package examen.calculadora_diegofernandez;
 public class Calculadora_DiegoFernandez {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Bienbenidos a mi repositorio");
     }
 }
